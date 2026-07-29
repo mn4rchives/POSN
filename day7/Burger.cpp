04083f76b225bfa9088b0bb27fc67a3399b6641e
mn4rchives <huacheng.26210@gmail.com>
2026-07-29 20:01:40 +0700

    update

--- day7/Burger.cpp
+++ day7/Burger.cpp
@@ -6,15 +6,31 @@ int n;
 vector<int>ind, v1, v2;
 
 bool check(int mid){
+    int lo=0, hi=mid;
     for(int i=0; i<n; i++){
+        int x;
+        int num = ind[i]-(v2[i]*mid);
 
+        if(v1[i]-v2[i]==0){
+            if(num<0) return false;
+        }else if(v1[i]>v2[i]){
+            x = num/(v1[i]-v2[i]);
+            hi = min(hi, x);
+
+        }else if(v2[i]>v1[i]){
+            x = ceil((float)num/(float)(v1[i]-v2[i]));
+            lo = max(lo, x);
+        }
+        if(lo>hi) return false;
     }
-    return true;
+    return lo<=hi;
 }
 
 int32_t main(){
     cin.tie(0);
     ios::sync_with_stdio(0);
+
+    cin>>n;
 
     ind.resize(n);
     v1.resize(n);
@@ -29,7 +45,7 @@ int32_t main(){
     for(int i=0; i<n; i++){
         cin>>v2[i];
     }
-    int lo=0, hi=2e9, ans=hi;
+    int lo=0, hi=1e9, ans=hi;
     while(lo<=hi){
         int mid = (lo+hi)/2;
 
@@ -46,5 +62,3 @@ int32_t main(){
 
     return 0;  
 }
-
-// x*v1 + (mid-x) *v2 <= ind;
